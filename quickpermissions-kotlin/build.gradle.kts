07b3272d5c76d35733e91cb66904dc6ec340c375
Niko Diamadis <niko@cyb3rko.de>
2024-09-02 12:15:38 +0200

    Configure maven-publish

--- quickpermissions-kotlin/build.gradle.kts
+++ quickpermissions-kotlin/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinter) // lintKotlin, formatKotlin
+    alias(libs.plugins.maven.publish)
 }
 
 android {
